06f94ff6f6fa7c466a75f02e04f6ef2d278a768a
Gleb <164905074+gleb7499@users.noreply.github.com>
2025-12-24 17:47:11 +0300

    Удалены работы Бурака и Попова

--- Popov/apps/web/src/pages/AdminUsersPage.tsx
+++
@@ -1,8 +0,0 @@
-export function AdminUsersPage() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Управление пользователями</h1>
-      <p>Страница в разработке</p>
-    </div>
-  );
-}
